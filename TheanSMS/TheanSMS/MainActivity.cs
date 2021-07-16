a7b3ed3845d7156d1b58697409bf084491081f58
Arunoyour <arunoyour@gmail.com>
2021-07-16 06:29:48 +0530

    Base code

---
+++ TheanSMS/TheanSMS/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Telephony;
+using Android.Widget;
+using Android.Views;
+using Android.Content;
+
+namespace TheanSMS
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme",Icon ="@drawable/Modified", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private static bool flag = false;
+        Button StartBtn, Stopbtn;
+        TextView txt;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            StartService(new Android.Content.Intent(this, typeof(TheanService)));
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            StartBtn =FindViewById<Button>(Resource.Id.startSelectingText);
+            StartBtn.Click += delegate { StartBtnClick(); };
+            Stopbtn = FindViewById<Button>(Resource.Id.stopSelectingText);
+            Stopbtn.Click += delegate { StopBtnClick(); };
+            txt = FindViewById<TextView>(Resource.Id.txt);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void StartBtnClick()
+        {
+            Intent intent = new Intent(this, typeof(TheanService));
+            this.StartService(intent);
+        }
+        public void StopBtnClick()
+        {
+            Intent intent = new Intent(this, typeof(TheanService));
+            this.StopService(intent);
+        }
+        public void SentSMS()
+        {
+            while(!flag)
+            {
+                //txt.Text = System.DateTime.Today.ToLongTimeString();
+                //SmsManager.Default.SendTextMessage("9539536943", null, "hai", null, null);
+                //System.Threading.Thread.Sleep(30000);
+            }
+        }
+    }
+}
---
+++ TheanSMS/TheanSMS/TheanService.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Telephony;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Util;
+using System.Threading;
+
+namespace TheanSMS
+{
+    [Service]
+    class TheanService : Service
+    {
+        private Timer _TimerData;
+        public override IBinder OnBind(Intent intent)
+        {
+          
+            return null;
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            Log.Debug("Thean", "Stating....");
+            SentSMS();
+            return StartCommandResult.NotSticky;
+            /*
+            Toast.MakeText(this, "**** Started *****", ToastLength.Long).Show();
+            bool flag = false;
+            while (!flag)
+            {
+                SmsManager.Default.SendTextMessage("9539536943", null, "hai", null, null);
+                System.Threading.Thread.Sleep(30000);
+            }
+            return StartCommandResult.NotSticky;
+            */
+        }
+
+        public void SentSMS()
+        {
+            _TimerData = new Timer((o)=> { sent(); },null,0, 30000);
+        }
+
+        public void sent()
+        {
+            SmsManager.Default.SendTextMessage("9539536943", null, "hai", null, null);
+           // Toast.MakeText(this, "**** Started *****", ToastLength.Long).Show();
+        }
+
+        public override void OnDestroy()
+        {
+            Toast.MakeText(this, "Stopped !!!", ToastLength.Long).Show();
+
+        }
+        public override bool StopService(Intent name)
+        {
+            return base.StopService(name);  
+        }
+    }
+}
